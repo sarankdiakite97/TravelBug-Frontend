d398bcddd1d2584d77687db92294556002075a80
sarankdiakite97 <sarankdiakite97@gmail.com>
2022-12-21 08:32:47 +0530

    updated

---
+++ src/API.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+var baseURL;
+// if (process.env.REACT_APP_ENVIRONMENT && process.env.REACT_APP_ENVIRONMENT === 'PRODUCTION') {
+//     baseURL = process.env.REACT_APP_API_BASE_URL;
+
+// baseURL = 'http://127.0.0.1:8000/';
+
+// baseURL = "";
+const api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export default class API {
+    getPlaces = async (search, category) => {
+        let url = '/places/';
+        let query = new URLSearchParams();
+        if (search) {
+            query.append('search', search);
+        }
+        if (category) {
+            query.append('category', category);
+        }
+
+        if (query.toString() != '') {
+            url += '?' + query.toString();
+        }
+
+        const places = await api
+            .get(url)
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                throw new Error(error);
+            });
+        return places;
+    };
+    getCategories = async () => {
+        const categories = await api
+            .get('/categories/')
+            .then(response => {
+                return response.data;
+            })
+            .catch(error => {
+                throw new Error(error);
+            });
+        return categories;
+    };
+}
